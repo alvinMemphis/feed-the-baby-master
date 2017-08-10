c23670ffb1c1cc31198b6ee705d465428d05ebac
alvinMemphis <alvinnsongambi9@gmail.com>
2017-08-10 18:39:11 +0300

    adding mpandroid graph

--- app/src/main/java/com/example/alvin/feedthebaby/Fragment/ExploreFragment.java
+++ app/src/main/java/com/example/alvin/feedthebaby/Fragment/ExploreFragment.java
@@ -1,16 +1,26 @@
 package com.example.alvin.feedthebaby.Fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.alvin.feedthebaby.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,32 +28,57 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 public class ExploreFragment extends Fragment {
 
 
-
     public ExploreFragment() {
         // Required empty public constructor
     }
+
     View view;
-    LineGraphSeries<DataPoint> series;
+    LineChart lineChart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_explore, container, false);
-        double y, x;
-        x = -5.0;
+        lineChart = (LineChart) view.findViewById(R.id.lineChart);
 
-        GraphView graph = (GraphView) view.findViewById(R.id.graph);
-        series = new LineGraphSeries<DataPoint>();
-        for (int i = 0; i < 100; i++) {
+        lineChart = (LineChart) view.findViewById(R.id.lineChart);
+
+        ArrayList<Entry> yAXISsin = new ArrayList<>();
+        ArrayList<Entry> yAXIScos = new ArrayList<>();
+
+        double x = 0.0;
+        int numDataPoints = 1000;
+
+        for (int i = 0; i < numDataPoints; i++) {
+            float sinFunction = Float.parseFloat(String.valueOf(Math.sin(x)));
+            float cosFunction = Float.parseFloat(String.valueOf(Math.cos(x)));
             x = x + 0.1;
-            y = Math.sin(x);
-            series.appendData(new DataPoint(x, y), true, 100);
-        }
-        graph.addSeries(series);
+            float xEntry = Float.parseFloat(String.valueOf(x));
+            yAXISsin.add(new Entry(xEntry, sinFunction));
+            yAXIScos.add(new Entry(xEntry, cosFunction));
 
+
+        }
+        ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
+        LineDataSet lineDataSet1 = new LineDataSet(yAXIScos,"cos");
+        LineDataSet lineDataSet2 = new LineDataSet(yAXISsin,"sin");
+
+        lineDataSet1.setDrawCircles(false);
+        lineDataSet1.setColors(Color.RED);
+        lineDataSet2.setDrawCircles(false);
+        lineDataSet2.setColors(Color.BLUE);
+
+        lineDataSets.add(lineDataSet1);
+        lineDataSets.add(lineDataSet2);
+
+        lineChart.setData(new LineData(lineDataSets));
+        lineChart.setVisibleXRangeMaximum(65f);
+
+//can also try calling invalidate() to refresh the graph
+        lineChart.invalidate();
         return view;
     }
 
-}
 
+}
